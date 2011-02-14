c1e5b9374f70bac35d1fd36914f19f7e8b2088f2
Atul Varma <varmaa@gmail.com>
2011-02-14 17:38:35 -0500

    Added event-emitter tests.

---
+++ test/unit/event-emitter.js
@@ -0,0 +1,33 @@
+module("event-emitter");
+
+test("$.eventEmitter() tests", function() {
+  var emitter = jQuery.eventEmitter({
+    myMethod: function() { return 1; }
+  });
+  
+  ok(emitter.myMethod() == 1, "inheritance works");
+
+  var timesCalled = 0;
+  function myListener() { timesCalled++; }
+
+  emitter.on('foo', myListener);
+  equals(timesCalled, 0, "on() does not call listeners");
+
+  emitter.emit('foo');
+  equals(timesCalled, 1, "emit() calls listeners once");
+
+  emitter.emit('bar');
+  equals(timesCalled, 1, "emit() calls only relevant listeners");
+
+  emitter.removeListener('foo', myListener);
+  emitter.emit('foo');
+  equals(timesCalled, 1, "removeListener() works");
+
+  emitter.addListener('foo', myListener);
+  emitter.emit('foo');
+  equals(timesCalled, 2, "addListener() is a synonym for on()");
+  
+  emitter.removeAllListeners('foo');
+  emitter.emit('foo');
+  equals(timesCalled, 2, "removeAllListeners() works");
+});
