29620ebe73f39d129a365d52b4b952e8dd7652b1
Atul Varma <varmaa@gmail.com>
2011-02-14 17:21:25 -0500

    Reorganized utils tests

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -1,15 +1,23 @@
 module("utils");
 
-test("utils tests", function() {
-  ok(jQuery("#qunit-header").ancestor(1).get(0) === window.document.body,
-     "jQuery.ancestor() works w/ non-null result");
-  ok(jQuery("#qunit-header").ancestor(30) === null,
-     "jQuery.ancestor() works w/ null result");
-  jQuery("#qunit-header").overlay().remove();
-  ok(true, "jQuery.overlay() does not throw, and returns a jQuery");
-
+test("$.emit() tests", function() {
   var stuff = jQuery("<div></div>");
   stuff.emit("hello ", jQuery("<em>there</em>"), " dude");
   equal(stuff.html(), "hello <em>there</em> dude",
         "jQuery.emit() works");
+});
+
+test("$.overlay() tests", function() {
+  var overlay = jQuery("#qunit-header").overlay();
+  ok(overlay.hasClass("webxray-overlay"),
+     "overlay has the 'webxray-overlay' class");
+  ok(true, "overlay() does not throw, and returns a jQuery");
+  overlay.remove();
+});
+
+test("$.ancestor() tests", function() {
+  ok(jQuery("#qunit-header").ancestor(1).get(0) === window.document.body,
+     "jQuery.ancestor() works w/ non-null result");
+  ok(jQuery("#qunit-header").ancestor(30) === null,
+     "jQuery.ancestor() works w/ null result");
 });
