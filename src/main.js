d8e6c8061a198167d1f036dc5d6a9c8796519943
Atul Varma <varmaa@gmail.com>
2011-02-17 11:37:41 -0600

    Added HUD status messages to undo/redo.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@
    * functionality to the goggles, at least until we have the
    * real MixMaster tool ready. */
 
-  function CommandManager(focused) {
+  function CommandManager(hud, focused) {
     var undoStack = [];
     var redoStack = [];
+
+    function updateStatus(verb, command) {
+      // TODO: We're assuming that 'verb' and 'command' are both already
+      // HTML-escaped here, which isn't necessarily obvious. Might want
+      // to escape them just in case.
+      $(hud.overlay).html('<span>' + verb + ' ' + command.name + '.</span>');
+    }
 
     var self = {
       run: function(command) {
@@ -15,30 +22,36 @@
         undoStack.push(command);
         redoStack.splice(0);
         command.execute();
+        updateStatus('Busted', command);
       },
       undo: function() {
         if (undoStack.length) {
           focused.unfocus();
-          var undoneCommand = undoStack.pop();
-          redoStack.push(undoneCommand);
-          undoneCommand.undo();
-        }
+          var command = undoStack.pop();
+          redoStack.push(command);
+          command.undo();
+          updateStatus('Unbusted', command);
+        } else
+          $(hud.overlay).html('<span>Nothing left to undo!</span>');
       },
       redo: function() {
         if (redoStack.length) {
           focused.unfocus();
-          var redoneCommand = redoStack.pop();
-          undoStack.push(redoneCommand);
-          redoneCommand.execute();
-        }
+          var command = redoStack.pop();
+          undoStack.push(command);
+          command.execute();
+          updateStatus('Rebusted', command);
+        } else
+          $(hud.overlay).html('<span>Nothing left to redo!</span>');
       }
     };
     
     return self;
   }
 
-  function ReplaceWithCmd(elementToReplace, newContent) {
+  function ReplaceWithCmd(name, elementToReplace, newContent) {
     return {
+      name: name,
       execute: function() {
         $(elementToReplace).replaceWith(newContent);
       },
@@ -83,7 +96,8 @@
             html = '<span>' + html + '</span>';
           }
           var newContent = $(html);
-          commandManager.run(ReplaceWithCmd(elementToReplace, newContent));
+          commandManager.run(ReplaceWithCmd('replacement', elementToReplace,
+                                            newContent));
         }
       }
       return true;
@@ -92,7 +106,8 @@
       var elementToDelete = focused.ancestor || focused.element;
       if (elementToDelete) {
         var placeholder = $('<span style="display: none;"></span>');
-        commandManager.run(ReplaceWithCmd(elementToDelete, placeholder));
+        commandManager.run(ReplaceWithCmd('deletion', elementToDelete,
+                                          placeholder));
       }
       return true;
     }
@@ -103,7 +118,7 @@
   $(window).ready(function() {
     var hud = jQuery.hudOverlay();
     var focused = jQuery.focusedOverlay();
-    var commandManager = CommandManager(focused);
+    var commandManager = CommandManager(hud, focused);
 
     $(document.body).append(hud.overlay);
     focused.on('change', hud.onFocusChange);
