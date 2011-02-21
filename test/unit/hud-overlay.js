49509fde2628a4b17dc5453b66263f93a93924c9
Atul Varma <varmaa@gmail.com>
2011-02-21 15:21:42 -0600

    Added hudOverlay.onFocusChange() tests.

--- test/unit/hud-overlay.js
+++ test/unit/hud-overlay.js
@@ -6,3 +6,44 @@ test("jQuery.hudOverlay() defaultContent works", function() {
   equals($(hud.overlay).html(), "hai2u");
   hud.destroy();
 });
+
+test("onFocusChange()", function() {
+  var $ = jQuery;
+  var hud = $.hudOverlay();
+  var focused = {
+    element: document.createElement('span'),
+    ancestor: null
+  };
+
+  hud.onFocusChange(focused);
+  equals($(hud.overlay).html(), "<span>You are on a <span><code>&lt;" +
+         "span&gt;</code> element</span>.</span>");
+
+  focused.element.id = "barg";
+  hud.onFocusChange(focused);
+  equals($(hud.overlay).html(), "<span>You are on a <span><code>&lt;" +
+         "span&gt;</code> element with id <code>barg</code>" +
+         "</span>.</span>");
+
+  focused.element.id = null;
+  focused.ancestor = document.createElement('div');
+  hud.onFocusChange(focused);
+  equals($(hud.overlay).html(), "<span>You are on a <span><code>&lt;" +
+         "span&gt;</code> element</span>. It is inside a <span>" +
+         "<code>&lt;div&gt;</code> element</span>.</span>");
+
+  focused.element.className = "boop";
+  focused.ancestor = null;
+  hud.onFocusChange(focused);
+  equals($(hud.overlay).html(), "<span>You are on a <span><code>&lt;" +
+         "span&gt;</code> element with class <code>boop</code>" +
+         "</span>.</span>");
+
+  focused.element.id = "barg";
+  hud.onFocusChange(focused);
+  equals($(hud.overlay).html(), "<span>You are on a <span><code>&lt;" +
+         "span&gt;</code> element with id <code>barg</code> and " +
+         "class <code>boop</code></span>.</span>");
+
+  hud.destroy();
+});
