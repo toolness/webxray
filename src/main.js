fc6fcff3d3f58f2c2921b56a5aff782757c4cf66
Atul Varma <varmaa@gmail.com>
2011-05-11 21:03:58 -0700

    Simplified bookmarklet code to only include a script tag (no more link tag), and also made the bookmarklet remove its script and link tags once it's unloaded.

--- src/main.js
+++ src/main.js
@@ -2,30 +2,51 @@
   "use strict";
 
   var $ = jQuery;
+  var removeOnUnload = $();
+
+  function loadPrerequisites(cb) {
+    var script = $("script.webxray");
+    var baseURI = script.attr("src").match(/(.*)webxray\.js$/)[1];
+    var cssURI = baseURI + 'webxray.css';
+    var cssLink = $('link[href="' + cssURI + '"]');
+
+    // This is a test to see if we're using legacy bookmarklet code,
+    // which inserts the link tag itself.
+    if (cssLink.length == 0) {
+      cssLink = $('<link rel="stylesheet" class="webxray"></link>');
+      $(document.head).append(cssLink.attr("href", cssURI));
+    }
+
+    removeOnUnload = removeOnUnload.add([cssLink.get(0), script.get(0)]);
+    cb();
+  }
 
   $(window).ready(function() {
-    var hud = jQuery.hudOverlay();
-    var focused = jQuery.focusedOverlay();
-    var mixMaster = jQuery.mixMaster({
-      hud: hud,
-      focusedOverlay: focused
-    });
-    var input = jQuery.xRayInput({
-      focusedOverlay: focused,
-      mixMaster: mixMaster
-    });
+    loadPrerequisites(function() {
+      var hud = jQuery.hudOverlay();
+      var focused = jQuery.focusedOverlay();
+      var mixMaster = jQuery.mixMaster({
+        hud: hud,
+        focusedOverlay: focused
+      });
+      var input = jQuery.xRayInput({
+        focusedOverlay: focused,
+        mixMaster: mixMaster
+      });
 
-    $(document.body).append(hud.overlay);
-    focused.on('change', hud.onFocusChange);
-    input.activate();
-    $(document.defaultView).focus();
+      $(document.body).append(hud.overlay);
+      focused.on('change', hud.onFocusChange);
+      input.activate();
+      $(document.defaultView).focus();
 
-    $(window.document).unload(function() {
-      focused.destroy();
-      focused = null;
-      input.deactivate();
-      hud.destroy();
-      hud = null;
+      $(window.document).unload(function() {
+        focused.destroy();
+        focused = null;
+        input.deactivate();
+        hud.destroy();
+        hud = null;
+        removeOnUnload.remove();
+      });
     });
   });
 })(jQuery);
--- static-files/bookmarklet.js
+++ static-files/bookmarklet.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function getGogglesBookmarkletURL(baseURI) {
-  var baseCode = '(function(){"use strict";var base="http://localhost:8000/";var link=document.createElement("link");link.setAttribute("rel", "stylesheet");link.setAttribute("href",base+"webxray.css");document.documentElement.appendChild(link);var script=document.createElement("script");script.src=base+"webxray.js";script.className="webxray";document.documentElement.appendChild(script);})();';
+  var baseCode = '(function(){"use strict";var script=document.createElement("script");script.src="http://localhost:8000/webxray.js";script.className="webxray";document.head.appendChild(script);})();';
 
   baseURI = baseURI || document.baseURI;
   return 'javascript:' + baseCode.replace('http://localhost:8000/', baseURI);
