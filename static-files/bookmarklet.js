8376712e4de2889fd715c3c25af4eef4a6f1b987
Atul Varma <varmaa@gmail.com>
2011-05-28 16:03:05 +0200

    Make bookmarklet.js declare 'use strict' in the getGogglesBookmarkletURL() function, not at global scope.

--- static-files/bookmarklet.js
+++ static-files/bookmarklet.js
@@ -1,6 +1,6 @@
-"use strict";
-
 function getGogglesBookmarkletURL(baseURI) {
+  "use strict";
+
   var baseCode = '(function(){"use strict";var script=document.createElement("script");script.src="http://localhost:8000/webxray.js";script.className="webxray";document.head.appendChild(script);})();';
 
   baseURI = baseURI || document.baseURI;
