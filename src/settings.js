4931e69c28e2f10e9b607f942e2ef12334256734
Atul Varma <varmaa@gmail.com>
2011-06-08 17:10:16 -0500

    Added jQuery.webxraySettings.url(), .extend(), and tests for settings.js.

--- src/settings.js
+++ src/settings.js
@@ -2,6 +2,13 @@
   "use strict";
   
   jQuery.webxraySettings = {
+    extend: jQuery.extend,
+    url: function(name) {
+      if (this[name].match(/^https?:/))
+        return this[name];
+      return this.baseURI + this[name];
+    },
+    baseURI: "",
     mixMasterDialogURL: "http://labs.toolness.com/dom-tutorial/"
   };
 })(jQuery);
