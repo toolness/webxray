55288707c2e30d4cdaa5ab4d040cbce7c4f67e5a
Atul Varma <varmaa@gmail.com>
2011-06-13 08:35:27 -0500

    the css info display now colors the properties specially if they are the same value as CSS rules applied to the focused element, or inline styles. the former doesn't work very well in the wild, though, because a lot of sites get their CSS from a cross-domain CDN, so the goggles can't introspect their CSS rules.

--- src/style-info.js
+++ src/style-info.js
@@ -35,6 +35,16 @@
     ]
   };
 
+  function anyRuleMatches(rules, name, value) {
+    for (var i = 0; i < rules.length; i++) {
+      var rule = rules[i];
+      
+      if (rule.style.getPropertyValue(name) == value)
+        return true;
+    }
+    return false;
+  }
+
   jQuery.extend({
     styleInfoOverlay: function styleInfoOverlay(options) {
       var focused = options.focused;
@@ -54,6 +64,7 @@
       function refresh() {
         overlay.empty();
         var primary = focused.getPrimaryElement();
+        
         if (primary) {
           var info = $(primary).getStyleInfo();
           overlay.append(info);
@@ -77,6 +88,33 @@
   });
   
   jQuery.fn.extend({
+    getMatchingCssRules: function getRelevantCssRules() {
+      var allMatches = [];
+      this.each(function() {
+        var element = this;
+        var doc = this.ownerDocument;
+        jQuery.each(document.styleSheets, function() {
+          var sheet = this;
+          if (!sheet.cssRules)
+            return;
+          jQuery.each(sheet.cssRules, function() {
+            var rule = this;
+            if (rule.type == rule.STYLE_RULE) {
+              var matches = $(rule.selectorText, doc);
+
+              var matches = doc.querySelectorAll(rule.selectorText);
+              
+              for (var i = 0; i < matches.length; i++)
+                if (matches[i] === element) {
+                  allMatches.push(rule);
+                  return;
+                }
+            }
+          });
+        });
+      });
+      return allMatches;
+    },
     getStyleInfo: function getStyleInfo() {
       var element = this.get(0);
       var window = element.ownerDocument.defaultView;
@@ -84,6 +122,7 @@
 
       var info = $('<div class="webxray-rows"></div>');
       var names = [];
+      var matchingCssRules = this.getMatchingCssRules();
 
       jQuery.each(style, function() {
         var name = this;
@@ -99,8 +138,8 @@
         });
         if (!isNameValid)
           return;
-        var value = style.getPropertyValue(name);
 
+        //var value = style.getPropertyValue(name);
         //if (value.match(/^(none|auto|normal)/))
         //  return;
 
@@ -115,10 +154,15 @@
         var row = $('<div class="webxray-row"></div>');
         for (var j = 0; j < NUM_COLS; j++) {
           var name = names[i+j];
+          var value = style.getPropertyValue(name);
           var nameCell = $('<div class="webxray-name"></div>');
           var valueCell = $('<div class="webxray-value"></div>');
           nameCell.text(name);
-          valueCell.text(style.getPropertyValue(name));
+          valueCell.text(value);
+          if (anyRuleMatches(matchingCssRules, name, value))
+            valueCell.addClass("webxray-value-matches-css-rule");
+          if (element.style.getPropertyValue(name) == value)
+            valueCell.addClass("webxray-value-matches-inline-style");
           row.append(nameCell);
           row.append(valueCell);
         }
