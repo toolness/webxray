2e7d6de12d83adaf980de2c6697ed120b48b8972
esenabre <esenabre@gmail.com>
2011-06-22 11:23:25 -0700

    Added spanish localization, taken from https://github.com/esenabre/webxray/commit/b404081006746ec5a386e95b20640f62a32cd75f

---
+++ src/locale/es.js
@@ -0,0 +1,37 @@
+jQuery.localization.extend("es", "hud-overlay", {
+  "default-html": "<span>Gafas de Rayos X activadas! Haz clic en ESC para desactivarlas.</span>",
+  "element": "elemento",
+  "with": "con",
+  "id": "id",
+  "and": "y",
+  "class": "clase",
+  "pointing-at": "apuntando a",
+  "focused-intro": "Estás en un",
+  "ancestor-intro": "Está dentro de"
+});
+
+jQuery.localization.extend("es", "input", {
+  "link-click-blocked": 'Si quieres seguir ese link, desactiva antes las gafas presionando ESC.'
+});
+
+jQuery.localization.extend("es", "hack-recording-playback", {
+  "success": 'Hackeado!',
+  "failure": 'Hackeo fallido. Tal vez la página cambió desde el primer hackeo?'
+});
+
+jQuery.localization.extend("es", "command-manager", {
+  "executed": "Ejecutado",
+  "undid": 'Sin hackear',
+  "redid": 'Hackeado',
+  "cannot-undo-html": '<span>No queda nada que desacer!</span>',
+  "cannot-redo-html": '<span>No queda nada que rehacer!</span>'
+});
+
+jQuery.localization.extend("es", "mix-master", {
+  "too-big-to-change": "Cambiar eso sería una mala idea.",
+  "too-big-to-remix-html": "<div>El elemento <code>&lt;${tagName}&gt;</code> es demasiado grande para remezclarlo. Selecciona uno más pequeño!</div>",
+  "deletion": "borrado",
+  "replacement": "substitución",
+  "compose-a-replacement": "Haz una substitución",
+  "replacement-instructions-html": "<span>Cuando hayas hecho tu substitución de HTML, presiona el botón <strong>Ok</strong>.</span>"
+});
