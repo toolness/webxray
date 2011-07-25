695ef797ac54ecabbfaf500efbe8d056922a7ff6
Atul Varma <varmaa@gmail.com>
2011-07-25 16:17:00 -0400

    The draggable attribute was messing up the way text caret positioning worked on chrome and firefox in various ways, and since it required so many instructions to understand, i've just removed it for now.
    The pretty view now only lets you edit existing text nodes and attribute values. Doing anything else must be done in the raw view.

--- static-files/easy-remix-dialog/edit-dom.js
+++ static-files/easy-remix-dialog/edit-dom.js
@@ -3,71 +3,41 @@
   
   var $ = jQuery;
 
-  var EXTRA_WIDTH = 16;
-  
-  function setFieldSize(field, element, text) {
-    var target = element.clone();
-    var br = $('<br></br>');
-    target.text(text);
-    field.after(target);
-    field.after(br);
-    field.width(target.outerWidth() + EXTRA_WIDTH);
-    field.height(target.outerHeight());
-    br.remove();
-    target.remove();
-  }
-
   jQuery.fn.extend({
     makeTextEditable: function makeTextEditable() {
       this.click(function(event) {
         var target = $(event.target);
-
-        if (target.is('textarea'))
+        
+        if ($(event.target).attr("contentEditable") != "inherit")
           return;
         
-        var form = $('<form></form>');
-        var input = $('<textarea></textarea>');
-        var linkedNode = target.data("linked-node");
         var originalValue = target.text();
-        input.val(originalValue);
-        form.append(input);
-        target.empty();
-        target.append(form);
-        setFieldSize(input, target, originalValue);
-        input.focus().select();
-        function onDone(event) {
+        var linkedNode = target.data("linked-node");
+        
+        $(event.target).attr("contentEditable", "true");
+
+        target.bind('keyup.editableText', function(event) {
           if (linkedNode)
-            linkedNode.nodeValue = input.val();
-          target.empty();
-          target.text(input.val());
-          input.unbind();
-          form.unbind();
-          event.preventDefault();
-          event.stopPropagation();
-          return false;
-        }
-        input.blur(onDone);
-        form.submit(onDone);
-        input.keyup(function(event) {
-          if (linkedNode)
-            linkedNode.nodeValue = input.val();
+            linkedNode.nodeValue = target.text();
         });
-        input.keypress(function(event) {
-          setFieldSize(input, target, input.val());
-        });
-        input.keydown(function(event) {
+        target.bind('keydown.editableText', function(event) {
           switch (event.keyCode) {
             case 27:
-            input.val(originalValue);
-            input.blur();
+            target.text(originalValue);
+            target.blur();
             return false;
 
             case 13:
-            input.blur();
+            target.blur();
             return false;
           }
           return true;
         });
+        target.bind('blur.editableText', function() {
+          target.unbind('.editableText');
+          target.attr("contentEditable", "inherit");
+        });
+        target.focus();
       });
       return this;
     }
