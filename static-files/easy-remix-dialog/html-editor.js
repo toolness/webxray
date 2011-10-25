be57d86de39fc93fde8f6636904eb3bd392d397f
Atul Varma <varmaa@gmail.com>
2011-10-25 14:06:11 -0700

    Attempted to fix goggles #77, 'Advanced' column of remix dialog encroaches on preview column.

--- static-files/easy-remix-dialog/html-editor.js
+++ static-files/easy-remix-dialog/html-editor.js
@@ -25,6 +25,12 @@
 
       $("#" + idToEdit).text(textContent);
 
+      var width = $(document.body).width() / 2.08;
+      var height = $(document.body).height() / 1.75;
+      
+      $("#" + idToEdit).parent().andSelf().width(width);
+      $("#" + idToEdit).parent().andSelf().height(height);
+
       // The default Mac keybindings map Command-L to 'go to line #',
       // which both isn't needed for this page and also overrides
       // the browser-default behavior of selecting the address bar,
@@ -40,7 +46,7 @@
       editor.renderer.setShowGutter(false);
       editor.setHighlightActiveLine(false);
       session.setUseWrapMode(true);
-      session.setWrapLimitRange(64, 64);
+      session.setWrapLimitRange(64, Math.floor(width / 7.8));
       session.getDocument().on("change", function() {
         onChange(getText());
       });
