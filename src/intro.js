1730bfd688718764471f02769f06db543dab40bf
Atul Varma <varmaa@gmail.com>
2011-11-18 05:41:15 -0800

    Create our own private JSON variable in our intro closure to ensure that we don't destructively modify window.JSON.

--- src/intro.js
+++ src/intro.js
@@ -8,3 +8,9 @@ var document = window.document;
 // This value is computed at build time.
 var buildMetadata = __BUILD_METADATA__;
 
+// We might be monkeypatching JSON later; if we do, ensure it's
+// our own private copy of JSON rather than the page's global one.
+var JSON = {
+  stringify: window.JSON && window.JSON.stringify,
+  parse: window.JSON && window.JSON.parse
+};
