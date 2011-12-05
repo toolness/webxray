b835c417296760d85cfe64b15c741360f98eb7d3
Atul Varma <varmaa@gmail.com>
2011-12-05 13:39:19 -0800

    Replaced the Ace widget with CodeMirror, as specified in [#102].
    Among other things, this simplifies the code, allows syntax highlighting to work on Opera and touch-based devices, and appears to fix [#81] at least insofar as the advanced source editor is concerned. The responsive CSS layout that Anna made works great using CodeMirror.

--- static-files/easy-remix-dialog/html-editor.js
+++ static-files/easy-remix-dialog/html-editor.js
@@ -1,62 +1,20 @@
 (function (jQuery) {
   var $ = jQuery;
-
-  function browserSupportsAce() {
-    return (!jQuery.browser.opera &&
-            navigator.userAgent.indexOf('Mobile') == -1 &&
-            navigator.userAgent.indexOf('Fennec') == -1);
-  }
   
   jQuery.HtmlEditor = function (idToEdit, textContent, onChange) {
-    function setupFallbackWidget() {
-      var textarea = $('<textarea class="fallback"></textarea>');
-      $("#" + idToEdit).append(textarea);
-      textarea.val(textContent).keyup(function() {
-        onChange($(this).val());
-      }).keyup();
-    }
-
-    // The interface of Ace widgets doesn't seem to be very well-defined,
-    // this could break if/when we upgrade Ace.
-    function setupAceWidget() {
-      function getText() {
-        return editor.getSession().getDocument().getValue();
+    var editor = CodeMirror(function(element) {
+      $("#" + idToEdit).replaceWith(element);
+    }, {
+      mode: "text/html",
+      theme: "jsbin",
+      tabMode: "indent",
+      value: textContent,
+      lineWrapping: true,
+      onChange: function() {
+        onChange(editor.getValue());
       }
+    });
 
-      $("#" + idToEdit).text(textContent);
-
-      var width = $(document.body).width() / 2.08;
-      var height = $(document.body).height() / 1.75;
-      
-      $("#" + idToEdit).parent().andSelf().width(width);
-      $("#" + idToEdit).parent().andSelf().height(height);
-
-      // The default Mac keybindings map Command-L to 'go to line #',
-      // which both isn't needed for this page and also overrides
-      // the browser-default behavior of selecting the address bar,
-      // so we'll disable it.
-      var mac = require("ace/keyboard/keybinding/default_mac");
-      delete mac.bindings.gotoline;
-
-      var editor  = ace.edit(idToEdit);
-      editor.setTheme("ace/theme/eclipse");
-      var HTMLMode = require("ace/mode/html").Mode;
-      var session = editor.getSession();
-      session.setMode(new HTMLMode());
-      editor.renderer.setShowGutter(false);
-      editor.setHighlightActiveLine(false);
-      session.setUseWrapMode(true);
-      session.setWrapLimitRange(64, Math.floor(width / 7.8));
-      session.getDocument().on("change", function() {
-        onChange(getText());
-      });
-
-      onChange(getText());
-    }
-
-    if (browserSupportsAce())
-      setupAceWidget();
-    else
-      setupFallbackWidget();
+    onChange(editor.getValue());
   }
 })(jQuery);
