b8d6b3a9e82162ea352641255df070693ca9e726
Atul Varma <varmaa@gmail.com>
2012-01-24 17:29:29 -0800

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,21 @@
-from fabric.api import *
+import os
 
-PROJ_ROOT = '~/webxray'
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+def lexists(path):
+    with settings(warn_only=True):
+        return local('test -e %s' % path).succeeded
 
 @task
 def deploy():
-    with cd(PROJ_ROOT):
-        run('git pull')
-        run('python go.py compile')
+    with lcd(ROOT):
+        if not lexists('locale'):
+            local('git clone https://github.com/hackasaurus/hackasaurus-locales.git locale')
+        local('python go.py compilemessages')
+        local('python go.py compile')
+        rsync_project(remote_dir='/var/webxray.hackasaurus.org',
+                      local_dir='static-files/',
+                      extra_opts='--copy-links')
