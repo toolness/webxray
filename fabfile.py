708efb7ea0991db7eafd08eb8355ee55e0b20455
Atul Varma <varmaa@gmail.com>
2012-02-02 16:36:56 -0500

    added an 'experiment' target to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -9,13 +9,23 @@ def lexists(path):
     with settings(warn_only=True):
         return local('test -e %s' % path).succeeded
 
-@task
-def deploy():
+def _deploy(dirname, url):
     with lcd(ROOT):
         if not lexists('locale'):
             local('git clone https://github.com/hackasaurus/hackasaurus-locales.git locale')
         local('python go.py compilemessages')
         local('python go.py compile')
-        rsync_project(remote_dir='/var/webxray.hackasaurus.org',
+        rsync_project(remote_dir=dirname,
                       local_dir='static-files/',
                       extra_opts='--copy-links')
+    print "Deployed to %s." % url
+
+@task
+def experiment(name):
+    _deploy(dirname='/var/www/temp/%s' % name,
+            url='http://labs.toolness.com/temp/%s/' % name)
+
+@task
+def deploy():
+    _deploy(dirname='/var/webxray.hackasaurus.org',
+            url='http://webxray.hackasaurus.org/')
